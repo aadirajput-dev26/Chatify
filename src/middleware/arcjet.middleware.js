288d59aae3ecf1aabcd36696e71eb1383fe64d9e
Aadityaraj Singh Mandloi <aadityarajsinghmandloi104@gmail.com>
2026-02-28 22:45:08 +0530

    Added bot protection in Arcjet

--- src/middleware/arcjet.middleware.js
+++ src/middleware/arcjet.middleware.js
@@ -5,15 +5,15 @@ export const arcjetMiddleware = async (req, res, next) => {
     try {
         const decision = await aj.protect(req);
         if (decision.isDenied()) {
-            // if(decision.reason.isBot) {
-            //     return res.status(403).json({ message: "Forbidden: Bot detected" });
-            //}else 
-            if(decision.reason.isRateLimit()) {
+            if(decision.reason.isBot) {
+                return res.status(403).json({ message: "Forbidden: Bot detected" });
+            }else 
+if(decision.reason.isRateLimit()) {
                 return res.status(429).json({ message: "Rate limit has been exceeded. Please try again later." });
             } 
-            // else {
-            //     return res.status(403).json({ message: "Forbidden: Request blocked due to security reasons" });
-            // }
+            else {
+                return res.status(403).json({ message: "Forbidden: Request blocked due to security reasons" });
+            }
         }
 
         if(decision.results.some(isSpoofedBot)){
